a224c8a3eb28c59a27879d1b0501ff170f0c24a0
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2024-10-25 15:29:26 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <>
 <div className="text-center py-10 bg-primaryDark">
     <a href="#" className="flex items-center justify-center mb-5 text-2xl font-semibold text-gray-100">
-        Aryan Panchal's Portfolio
+        Aryan Panchal | Portfolio
     </a>
 
     <span className="block text-sm text-center text-gray-500">© 2021-2022. All Rights Reserved.        
