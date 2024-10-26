3e94aa3716a6826773720850332d32d6952820dc
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2024-10-26 18:35:23 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,8 @@ export const metadata: Metadata = {
   description: "As a Computer Engineer with over 3 years of corporate exposure and 2 years of technical experience, I have exhibited problem solving skills and technical acumen. My polished communication skills, coupled with extensive leadership experience, have been pivotal in propelling multiple startups and small-scale companies toward operational excellence and scalability.",
 };
 
+
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -28,10 +30,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
-        {/* Thumbnail Image */}
-        <a href="https://ibb.co/qp2yd67">
-          <img src="https://i.ibb.co/qp2yd67/Screenshot-2024-10-26-182613.png" alt="Thumbnail" />
-        </a>
         <Analytics />
       </body>
     </html>
