50a7c1c0ef26211971df3cd5cc01238790eba5e9
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2024-10-26 18:29:22 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
-import type { Metadata } from "next";
+import { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,11 +25,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
-        <Analytics />
+        {/* Thumbnail Image */}
+        <a href="https://ibb.co/qp2yd67">
+          <img src="https://i.ibb.co/qp2yd67/Screenshot-2024-10-26-182613.png" alt="Thumbnail" />
+        </a>
       </body>
     </html>
   );
