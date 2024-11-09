01bffe9a6733198bdb376a42a3794ccea7a9523e
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2024-11-09 23:46:36 +0530

    Update Work.tsx

--- components/Work.tsx
+++ components/Work.tsx
@@ -15,7 +15,7 @@ const Work = () => {
               <h1 className="mx-auto font-semibold text-lg text-white z-5">1</h1>
             </div>
             <div className="order-1 bg-primary rounded-lg shadow-xl w-full mobile:w-5/12 px-4 py-4">
-              <h3 className="font-bold text-white text-lg mobile:text-sm laptop:text-lg">Project Intern, Scanbo.com</h3>
+            <h3 className=" font-bold text-white text-lg mobile:text-sm laptop:text-lg">Project Intern, <span className='font-thin'>Scanbo.com</span></h3>
               {/* <p className="laptop:text-lg mobile:text-sm leading-snug text-white">
                 Developing deep learning models for hyperglycemia detection using ECG and PPG signals. 
               </p> */}
@@ -29,7 +29,7 @@ const Work = () => {
               <h1 className="mx-auto text-white font-semibold text-lg">2</h1>
             </div>
             <div className="order-1 bg-primary rounded-lg shadow-xl w-full mobile:w-5/12 px-4 py-4">
-              <h3 className=" font-bold text-white text-lg mobile:text-sm laptop:text-lg">Startup Engineer, Chemisphere.in</h3>
+            <h3 className=" font-bold text-white text-lg mobile:text-sm laptop:text-lg">Startup Engineer, <span className='font-thin'>Chemisphere.in</span></h3>
               {/* <p className="laptop:text-lg mobile:text-sm leading-snug text-white">
                 Leading the strategic design and development of Chemisphere website and application. Boosted social media engagement to over 250K members and managed intern training and skill development.
               </p> */}
@@ -43,7 +43,7 @@ const Work = () => {
               <h1 className="mx-auto font-semibold text-lg text-white">3</h1>
             </div>
             <div className="order-1 bg-primary rounded-lg shadow-xl w-full mobile:w-5/12 px-4 py-4">
-              <h3 className=" font-bold text-white text-lg mobile:text-sm laptop:text-lg">Founding President, E-Cell, RGIT</h3>
+              <h3 className=" font-bold text-white text-lg mobile:text-sm laptop:text-lg">Founding President, <span className='font-thin'>E-Cell, RGIT</span></h3>
               {/* <p className="laptop:text-lg mobile:text-sm leading-snug text-white">
                 Founded and led the E-Cell at RGIT, driving innovation and a startup ecosystem with over 100 students. 
               </p> */}
@@ -57,7 +57,7 @@ const Work = () => {
               <h1 className="mx-auto text-white font-semibold text-lg">4</h1>
             </div>
             <div className="order-1 bg-primary rounded-lg shadow-xl w-full mobile:w-5/12 px-4 py-4">
-              <h3 className=" font-bold text-white text-lg mobile:text-sm laptop:text-lg">Data Science Intern, AICTE</h3>
+            <h3 className=" font-bold text-white text-lg mobile:text-sm laptop:text-lg">Data Science Intern, <span className='font-thin'>AICTE</span></h3>
               {/* <p className="laptop:text-lg mobile:text-sm leading-snug text-white">
                 Engineered a machine learning model to forecast car prices, including data cleaning, preprocessing, and linear regression. 
               </p> */}
